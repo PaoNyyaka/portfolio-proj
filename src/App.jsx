bc93c86c5884b4078ef4804470ea8bc7daadccc4
Andrey <PaoNyao5@gmail.com>
2024-02-24 03:02:34 +0300

    Add loading icon and styles App.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styles from './App.module.css'
 
 function App() {
 
-  return ( <div> Hello World</div>
+  return ( <div className={styles.App}> Hello World</div>
   )
 }
 
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const getIgameUrl = (path) => {
+    return new URL(`assets/${path}`, import.meta.url).href;
+}
