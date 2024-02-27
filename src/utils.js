f3881e5d46f71647d61db763f7caa623e3ac870d
Andrey <PaoNyao5@gmail.com>
2024-02-27 23:26:59 +0300

    nav is done, well done. Prepare to Hello Section

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-export const getIgameUrl = (path) => {
-    return new URL(`assets/${path}`, import.meta.url).href;
-}
+export const getImageUrl = (path) => {
+    return new URL(`/assets/${path}`, import.meta.url).href;
+};
