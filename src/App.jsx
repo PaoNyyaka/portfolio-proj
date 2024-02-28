9feef3387641d9963973b9ba6aa7b075500f5097
Andrey <PaoNyao5@gmail.com>
2024-02-29 00:38:03 +0300

    Hero section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import styles from './App.module.css'
-import Navbar from './components/Navbar/Navbar'
+import styles from './App.module.css';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
 
   return ( <div className={styles.App}>
     <Navbar />
+    <Hero />
   </div>
   )
 }
