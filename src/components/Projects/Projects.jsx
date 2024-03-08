a2de38a21d625824f5f766cf2ca1a6943a0a7a10
Andrey <PaoNyao5@gmail.com>
2024-03-09 02:26:11 +0300

    Prepare arrows

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import projects from '../../data/projects.json'
 import styles from './Projects.module.css'
 import { ProjectCard } from './ProjectCard'
+import { getImageUrl } from '../../utils'
 
 export const Projects = () => {
     return (
         <section className={styles.container} id='projects'>
+             <div className={styles.arrows}>
+                <img src={getImageUrl('projects/arrow/left-arrow.png')} alt="arrow left" />
+            </div>
             <h2 className={styles.title}>Projects</h2>
             <div className={styles.projects}>
                 {projects.map((project, id) => {
@@ -14,6 +18,9 @@ export const Projects = () => {
                             );    
                     })
                 }
+            </div>
+            <div className={styles.arrows}>
+                <img src={getImageUrl('projects/arrow/right-arrow.png')} alt="arrow right" />
             </div>
         </section>
     )
