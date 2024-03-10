4f5cf6bd35681b4df5dfda8f035695d67ffc6321
Andrey <PaoNyao5@gmail.com>
2024-03-11 00:31:58 +0300

    Arrows logic and btn is 50\50

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import projects from '../../data/projects.json'
 import styles from './Projects.module.css'
 import { ProjectCard } from './ProjectCard'
 import { getImageUrl } from '../../utils'
 
 export const Projects = () => {
+    const projectsPerPage = 3;
+    const [startIndex, setStartIndex] = useState(0);
+
+    const handleNext = () => {
+        setStartIndex((prevIndex) => prevIndex + 1);
+    };
+
+    const handlePrev = () => {
+        setStartIndex((prevIndex) => prevIndex - 1);
+    };
+
     return (
         <section className={styles.container} id='projects'>
-             <div className={styles.arrows}>
-                <img src={getImageUrl('projects/arrow/left-arrow.png')} alt="arrow left" />
-            </div>
             <h2 className={styles.title}>Projects</h2>
-            <div className={styles.projects}>
-                {projects.map((project, id) => {
-                        return (
+            <div className={styles.imgContainer}>
+                <div className={styles.projects}>
+                    <div className={styles.btnLeft}>
+                        <button
+                            className={styles.btnLeftArrow}
+                            onClick={handlePrev}
+                            disabled={startIndex === 0}
+                        >
+                            <img
+                                src={getImageUrl('projects/arrow/left-arrow.png')}
+                                className={styles.BtnLeftImg}
+                                alt='arrow left'
+                            />
+                        </button>
+                    </div>
+                    <div className={styles.projectCards}>
+                        {projects.slice(startIndex, startIndex + projectsPerPage).map((project, id) => (
                             <ProjectCard key={id} project={project} />
-                            );    
-                    })
-                }
+                        ))}
+                    </div>
+                    <div className={styles.btnRight}>
+                        <button
+                            className={styles.btnRightArrow}
+                            onClick={handleNext}
+                            disabled={startIndex + projectsPerPage >= projects.length}
+                        >
+                            <img
+                                src={getImageUrl('projects/arrow/right-arrow.png')}
+                                className={styles.BtnRightImg}
+                                alt='arrow right'
+                                />
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className={styles.arrows}>
-                <img src={getImageUrl('projects/arrow/right-arrow.png')} alt="arrow right" />
+            <div className={styles.btnSmallContainer}>
+                <div className={styles.btnLeft_small}>
+                <button className={styles.btnLeftArrow}
+                        onClick={handlePrev}
+                        disabled={startIndex === 0}>
+                    <img
+                        src={getImageUrl('projects/arrow/left-arrow.png')}
+                        className={styles.BtnLeftImg}
+                        alt='arrow left'
+                    />
+                </button>
+                </div>
+                <div className={styles.btnRight_small}>
+                <button
+                 className={styles.btnRightArrow}
+                 onClick={handleNext}
+                 disabled={startIndex + projectsPerPage >= projects.length}
+                 >
+                    <img
+                        src={getImageUrl('projects/arrow/right-arrow.png')}
+                        className={styles.BtnRightImg}
+                        alt='arrow right'
+                    />
+                </button>
+                </div>
             </div>
         </section>
-    )
-}
+    );
+};
