0d13500c63e745ee74171763c1a476ca758e3368
Andrey <PaoNyao5@gmail.com>
2024-03-12 00:30:53 +0300

    Botton`s almost done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -21,63 +21,52 @@ export const Projects = () => {
             <h2 className={styles.title}>Projects</h2>
             <div className={styles.imgContainer}>
                 <div className={styles.projects}>
-                    <div className={styles.btnLeft}>
+                    <div className={`${styles.btnLeft} ${styles.btnSpan} ${styles.btnHide}`}>
                         <button
-                            className={styles.btnLeftArrow}
                             onClick={handlePrev}
                             disabled={startIndex === 0}
-                        >
-                            <img
-                                src={getImageUrl('projects/arrow/left-arrow.png')}
-                                className={styles.BtnLeftImg}
-                                alt='arrow left'
-                            />
+                        >Нажми влево
                         </button>
+                        <span></span>
+                        <span></span>
                     </div>
                     <div className={styles.projectCards}>
                         {projects.slice(startIndex, startIndex + projectsPerPage).map((project, id) => (
                             <ProjectCard key={id} project={project} />
                         ))}
                     </div>
-                    <div className={styles.btnRight}>
+                    <div className={`${styles.btnRight} ${styles.btnSpan} ${styles.btnHide}`}>
                         <button
-                            className={styles.btnRightArrow}
                             onClick={handleNext}
                             disabled={startIndex + projectsPerPage >= projects.length}
-                        >
-                            <img
+                        >Нажми вправо
+{/*                             <img
                                 src={getImageUrl('projects/arrow/right-arrow.png')}
                                 className={styles.BtnRightImg}
                                 alt='arrow right'
-                                />
+                                /> */}
                         </button>
+                        <span></span>
+                        <span></span>
                     </div>
                 </div>
             </div>
-            <div className={styles.btnSmallContainer}>
-                <div className={styles.btnLeft_small}>
-                <button className={styles.btnLeftArrow}
+            <div className={styles.mainContainerBtn}>
+                <div className={`${styles.btnLeft} ${styles.btnSpan}`}>
+                    <button
                         onClick={handlePrev}
-                        disabled={startIndex === 0}>
-                    <img
-                        src={getImageUrl('projects/arrow/left-arrow.png')}
-                        className={styles.BtnLeftImg}
-                        alt='arrow left'
-                    />
-                </button>
+                        disabled={startIndex === 0}
+                    >Нажми влево
+                    </button>
+                    <span></span>
+                    <span></span>
                 </div>
-                <div className={styles.btnRight_small}>
-                <button
-                 className={styles.btnRightArrow}
-                 onClick={handleNext}
-                 disabled={startIndex + projectsPerPage >= projects.length}
-                 >
-                    <img
-                        src={getImageUrl('projects/arrow/right-arrow.png')}
-                        className={styles.BtnRightImg}
-                        alt='arrow right'
-                    />
-                </button>
+                <div className={`${styles.btnRight} ${styles.btnSpan}`}>
+                    <button
+                        onClick={handleNext}
+                        disabled={startIndex + projectsPerPage >= projects.length}
+                    >Нажми вправо
+                    </button>
                 </div>
             </div>
         </section>
